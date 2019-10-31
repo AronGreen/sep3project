97673906ab676cb33ce983d15cd39e1bc86c3ec6
Konrad Piotrowski <konrad.piotrowski00@gmail.com>
2019-10-31 18:26:17 +0100

    Please ignore, nothing important is going on in here, don't expect fireworks

---
+++ FrontLayer/FrontEnd/Pages/Entities/Trip.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Pages.Entities
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+    }
+}
