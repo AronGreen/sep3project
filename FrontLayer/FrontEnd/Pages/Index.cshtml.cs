17991359434fdcdd0d7135cfb7f7c9384e4c251f
Aleksander Bialik <aleksanderbialik97@gmail.com>
2019-11-01 10:28:07 +0100

    Post and Get http requests

--- FrontLayer/FrontEnd/Pages/Index.cshtml.cs
+++ FrontLayer/FrontEnd/Pages/Index.cshtml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using FrontEnd.Pages.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace FrontEnd.Pages
 {
@@ -16,12 +19,22 @@ namespace FrontEnd.Pages
         }
 
 
-        static async Task<string> GetDataAsync(string address)
+        static async Task<string> GetTripAsync(string address)
         {
             HttpClient client = new HttpClient();
             Console.WriteLine("Fetching data...");
             string s = await client.GetStringAsync(address);
             return s;
+        }
+
+        static async Task<string> PostTrip(Trip trip, string address) {
+            var client = new HttpClient();
+            string jsonString = JsonConvert.SerializeObject(trip);
+            var content = new StringContent(jsonString, Encoding.UTF8);
+
+            HttpResponseMessage response = await client.PostAsync(address, content);
+
+            return response.ToString();
         }
     }
 }
