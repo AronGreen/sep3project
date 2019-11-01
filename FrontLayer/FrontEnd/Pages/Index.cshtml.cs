40da00cb54342f282c82bcd7ba78b2117f8913f6
Konrad Piotrowski <konrad.piotrowski00@gmail.com>
2019-11-01 09:27:53 +0100

    .save

--- FrontLayer/FrontEnd/Pages/Index.cshtml.cs
+++ FrontLayer/FrontEnd/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,6 +13,15 @@ namespace FrontEnd.Pages
         public void OnGet()
         {
 
+        }
+
+
+        static async Task<string> GetDataAsync(string address)
+        {
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Fetching data...");
+            string s = await client.GetStringAsync(address);
+            return s;
         }
     }
 }
