2b1877621cad0daeca27e19a0d9e6eccf3c08660
Aleksander Bialik <aleksanderbialik97@gmail.com>
2019-11-27 08:49:22 +0100

    Register account

--- FrontLayer/FrontEnd/Pages/Register.cshtml.cs
+++ FrontLayer/FrontEnd/Pages/Register.cshtml.cs
@@ -1,17 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
 namespace FrontEnd.Pages
 {
     public class RegisterModel : PageModel
+
     {
-        public void OnGet()
+        
+        public async Task OnPost()
         {
-          
-           
+            var email = Request.Form["email"];
+            var password = Request.Form["password"];
+            var firstName = Request.Form["firstName"];
+            var lastName = Request.Form["lastName"];
+            var dateOfBirth = Request.Form["dateOfBirth"];
+            var phoneNumber = Request.Form["phoneNumber"];
+
+           /* Account account = new Account
+            {
+                Email = email,
+                FirstName = firstName,
+                LastName = lastName,
+                Password = password,
+                DateOfBirth = dateOfBirth,
+
+                Phone = phoneNumber,
+                //PasswordSalt  //TODO
+
+
+            };
+
+            HttpClient client = new HttpClient();
+
+            var json = JsonConvert.SerializeObject(account);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync("http://localhost:8080/Logic_war_exploded/accounts/create", content);*/
+
+
+
+
         }
     }
 }
