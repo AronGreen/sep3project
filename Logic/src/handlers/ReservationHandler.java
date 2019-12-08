334c463e1669001d74cd783835859621f235f02a
Konrad Piotrowski <konrad.piotrowski00@gmail.com>
2019-12-08 21:30:13 +0100

    create reservation logic

--- Logic/src/handlers/ReservationHandler.java
+++ Logic/src/handlers/ReservationHandler.java
@@ -1,24 +1,57 @@
 package handlers;
 
+import constants.ResponseStatus;
+import helpers.StringHelper;
 import models.Reservation;
+import models.Trip;
 import models.response.ReservationListResponse;
 import models.response.ReservationResponse;
 import services.IReservationService;
+import services.ITripService;
 import services.ReservationService;
+import services.TripService;
+
+import java.util.List;
 
 public class ReservationHandler implements IReservationHandler {
 
     private IReservationService service;
+    private ITripService tripService;
 
     public ReservationHandler() {
         service = new ReservationService();
+        tripService = new TripService();
     }
 
     @Override
     public ReservationResponse create(Reservation reservation) {
         // No logic is implemented for now
 
-        return service.create(reservation);
+        Trip refTrip = tripService.getById(reservation.getTripId()).getBody();
+        List<Reservation> reservationList = service.getByTripId(refTrip.getId()).getBody();
+
+        // For now reservation allows to book only one seat.
+        // This implementation should be modified when the "bookedSeats" is added to Reservation object!
+
+        int availableSeats = refTrip.getTotalSeats();
+        for (Reservation res: reservationList) {
+            availableSeats--;
+        }
+
+        if(availableSeats>0 &&
+                StringHelper.isNullOrEmpty(reservation.getDropoffAddress()) &&
+                StringHelper.isNullOrEmpty(reservation.getPickupAddress()) &&
+                StringHelper.isNullOrEmpty(reservation.getPickupTime()) &&
+                StringHelper.isNullOrEmpty(reservation.getPassengerEmail())) {
+            return service.create(reservation);
+        }
+
+        ReservationResponse failed = new ReservationResponse();
+        failed.setBody(null);
+        failed.setStatus(ResponseStatus.SOCKET_FAILURE);
+        
+        return failed;
+
     }
 
     @Override
