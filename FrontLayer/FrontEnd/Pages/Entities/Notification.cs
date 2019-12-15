80f58ba2c84df41ba55dfc1d5801639999a93778
Konrad Piotrowski <konrad.piotrowski00@gmail.com>
2019-12-15 19:55:47 +0100

    hey hey, simple notifications view is ready

---
+++ FrontLayer/FrontEnd/Pages/Entities/Notification.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Pages.Entities
+{
+    public class Notification
+    {
+
+        /*
+         * When do we need notifications:
+         *  !- A pending reservation has been accepted (notify passenger)
+         *  !- A pending reservation has been rejected (notify passenger)
+         *  !- An approved reservation has been deleted (notify driver)
+         *  !- A reservation has been placed for a trip (notify driver)
+         *  !- A trip has been cancelled (notify passengers)
+         *  !- A payment has been issued (notify payer)
+         *  !- A payment has been revoked (notify payer)
+         *  !- A payment has been rejected (notify payer)
+         */
+
+       
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("accountEmail")]
+        public string AccountEmail { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("itemId")]
+        public int ItemId { get; set; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+    }
+}
---
+++ FrontLayer/FrontEnd/Pages/Notifications.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using FrontEnd.Pages.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontEnd.Pages
+{
+    public class NotificationsModel : PageModel
+    {
+
+        [BindPropertyAttribute]
+        public List<Notification> Notifications { get; set; }
+
+        string mail;
+
+        public async Task OnGet()
+        {
+            Notifications = new List<Notification>();
+            mail = Request.Cookies["EmailCookie"];
+            Thread thread = new Thread(async() => {
+                while (true)
+                {
+                    await OnPostNotificationAsync();
+                    Thread.Sleep(5000);
+                }
+            }
+            );
+            
+        }
+
+        private async Task OnPostNotificationAsync()
+        {
+
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Fetching data...");
+            var s = await client.GetStringAsync("http://localhost:8080/Logic_war_exploded/notifications/get/" + mail + "?limit=20");
+            List<Notification> temp = JsonSerializer.Deserialize<List<Notification>>(s);
+
+
+            Notifications = temp;
+        }
+    }
+}
